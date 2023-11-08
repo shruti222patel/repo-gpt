699e93eae261850b9e555f4a7c30dc85e9e7d4ce
Shruti Patel <shruti222patel@gmail.com>
2023-11-08 09:44:15 -0500

    Remove unused cli commands:

--- src/repo_gpt/cli.py
+++ src/repo_gpt/cli.py
@@ -1,5 +1,4 @@
 #!./venv/bin/python
-import os
 from pathlib import Path
 from typing import Union
 
@@ -72,37 +71,6 @@ def main():
     )
     parser_query.add_argument("question", type=str, help="Question to ask")
 
-    # Sub-command to analyze a file
-    analyze_file = subparsers.add_parser("analyze", help="Analyze a file")
-    analyze_file.add_argument("file_path", type=str, help="File to analyze")
-
-    # Sub-command to explain a file
-    explain_code = subparsers.add_parser("explain", help="Explain a code snippet")
-    explain_code.add_argument(
-        "--language", default="", type=str, help="Language of the code"
-    )
-    explain_code.add_argument("--code", type=str, help="Code you want to explain")
-
-    # Sub-command to analyze a file
-    add_test = subparsers.add_parser("add-test", help="Add tests for existing function")
-    add_test.add_argument(
-        "function_name", type=str, help="Name of the function you'd like to test"
-    )
-    add_test.add_argument(
-        "--file_name",
-        type=str,
-        help="Name of the file the function is found in. This is helpful if there are many functions with the same "
-        "name. If this isn't specified, I assume the function name is unique and I'll create tests for the first "
-        "matching function I find. When a file_name is passed, I will assume the function name is unique in the "
-        "file, and write tests for the first function I find with the same name in the file.",
-        default="",
-    )
-    add_test.add_argument(
-        "--test_save_file_path",
-        type=str,
-        help="Filepath to save the generated tests to",
-    )
-
     parser_help = subparsers.add_parser("help", help="Show this help message")
     parser_help.set_defaults(func=print_help)
 
@@ -135,39 +103,6 @@ def main():
         _, additional_args = parser_query.parse_known_args()
         repo_qna = RepoQnA(additional_args[0], args.code_root_path, args.pickle_path)
         repo_qna.initiate_chat()
-    elif args.command == "explain":
-        update_code_embedding_file(
-            search_service, args.code_root_path, args.pickle_path
-        )
-        _, additional_args = explain_code.parse_known_args()
-        search_service.explain(additional_args[0])
-    elif args.command == "analyze":  # TODO change to explain function
-        update_code_embedding_file(
-            search_service, args.code_root_path, args.pickle_path
-        )
-        _, additional_args = analyze_file.parse_known_args()
-        search_service.analyze_file(additional_args[0])
-    elif args.command == "explain":
-        search_service = SearchService(openai_service, language=args.language)
-        _, additional_args = explain_code.parse_known_args()
-        return search_service.explain(additional_args[0])
-    # elif args.command == "add-test":
-    #     update_code_embedding_file(
-    #         search_service, args.code_root_path, args.pickle_path
-    #     )
-    #     code_manager = CodeManager(Path(args.pickle_path), Path(args.code_root_path))
-    #     # Look for the function name in the embedding file
-    #     _, additional_args = add_test.parse_known_args()
-    #     # print(additional_args)
-    #
-    #     print("Adding tests is temporarily disabled due to a bug in the cli parser.")
-    #     add_tests(
-    #         search_service,
-    #         code_manager,
-    #         args.function_name,
-    #         args.test_save_file_path,
-    #         args.testing_package,
-    #     )
     else:
         parser.print_help()
 
@@ -178,41 +113,6 @@ def update_code_embedding_file(
     manager = CodeManager(Path(code_embedding_file_path), Path(root_file_path))
     manager.setup()
     search_service.refresh_df()
-
-
-def add_tests(
-    search_service,
-    code_manager,
-    function_name,
-    test_save_file_path,
-    testing_package,
-):
-    # TODO: add language & test framework from config file
-    # Check file path isn't a directory
-    if os.path.isdir(test_save_file_path):
-        print(
-            f"Error: {test_save_file_path} is a directory. Please specify a file path."
-        )
-        return
-
-    # TODO: check this. it seems wrong
-    code = update_code_embedding_file(search_service, code_manager, function_name)
-
-    # Save gpt history
-    # Ask gpt to explain the function
-    test_generator = TestGenerator(
-        code,
-        language="python",
-        unit_test_package=testing_package,
-        debug=True,
-    )
-    unit_tests = test_generator.unit_tests_from_function()
-
-    print(f"Writing generated unit_tests to {test_save_file_path}...")
-    # Save code to file
-    if test_save_file_path is not None:
-        with open(test_save_file_path, "a") as f:
-            f.write(unit_tests)
 
 
 if __name__ == "__main__":
